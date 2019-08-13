565aa9d339840268850012d63abb410aca1396bd
shivang-chhabra <shivangcool077@gmail.com>
2019-08-13 14:23:19 +0530

    firstcommit

---
+++ reviewnext/templatecard.js
@@ -0,0 +1,30 @@
+[{
+    "name": "TIM HORTIN'S",
+    "address":"123 north avenue citadell",
+    "description": "A beautifull place  in the centre of new york",
+    "price": "$20,000",
+    "img1":"",
+        "img2":"",
+    "img3":""
+
+},
+    {
+        "name": "JACKSON VILLA'S",
+        "address": "3A/21 south avenue new york",
+        "description": "A beautifull place  in the centre of new york",
+        "price": "$30,00",
+        "img1": "",
+        "img2": "",
+        "img3": ""
+    },
+    {
+        "name": "AMBANI TOWER'S",
+        "address": "31 main city chicago",
+        "description": "A beautifull place  in the centre of new york",
+        "price": "$40,000",
+        "img1": "",
+        "img2": "",
+        "img3": ""
+    }
+
+]
---
+++ reviewnext/templatedesc.js
@@ -0,0 +1,13 @@
+[{
+    "name1": "@amitaggarwal121",
+    "review1":"it is the best place to live in the vicinity of the city",
+    "img1": "",
+    "name2": "@zakirhussain45",
+    "review2": "it is quite spacious for a nuclear family.it has all the basic amenities for children.",
+    "img2": "",
+    "name3": "@rameshsippi414",
+    "img3": "",
+    "review3":"it is very beutifull and peacefull place to live ."
+}
+
+]
